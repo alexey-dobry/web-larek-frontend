c3afa7843f0d9e36edd0f6e79f7fce1ba25bbf87
Alexey Nehaenko <Nehaenkog23@gmail.com>
2025-03-05 10:45:42 +0300

    Added docs and basic interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,74 @@
+export interface IOrderForm {
+  items: string[];
+  payment: string;
+  total: number;
+  address: string;
+  email: string;
+  phone: string;
+}
+
+export interface IAppState {
+	catalog: IProduct[];
+	basket: string[];
+	preview: string;
+	order: IOrder;
+}
+
+export interface IFormState {
+	valid: boolean;
+	errors: string[];
+}
+
+export interface IProduct {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number;
+}
+
+export interface IOrderForm {
+	payment: string;
+	address: string;
+	email: string;
+	phone: string;
+}
+
+export interface IOrder {
+	total: number;
+	items: IOrderForm[];
+}
+
+export interface IOrderResult {
+	id: string;
+}
+
+export interface IPageView {
+	counter: number;
+	catalog: HTMLElement[];
+	loadProdusts(): void;
+	onClick(): void;
+}
+
+export interface ICardView {
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number;
+	onClick(): void;
+}
+
+export interface IBasketView {
+	products: HTMLElement[];
+	total: number;
+	deleteProduct(): void;
+	onClick(): void;
+}
+
+export interface IModalView {
+	content: HTMLElement;
+	open(): void;
+	close(): void;
+}
